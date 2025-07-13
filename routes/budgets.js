316fe0940be5b1599f6564ee3a250c5f94185b39
Vinski <vinski.andrade@visy.fi>
2025-07-13 15:57:05 +0300

    budgets end-point created

--- routes/budgets.js
+++ routes/budgets.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', function(req, res, next) {
-  res.json('Live Budget API');
+  res.json({ endPoint: '/budgets'});
 });
 
 module.exports = router;
